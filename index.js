389e0b35e604025da87d241beb2542cb0015f9f2
esteban nuñez <estebanleonel9812@gmail.com>
2023-02-11 16:00:14 -0300

    primera clase

---
+++ index.js
@@ -0,0 +1,39 @@
+//.log (javaseeecript)
+// mi primer comentarios
+
+//*vv8*/
+
+//var primeraVariable= 'hola'; //string " "
+//var segundaVariable = 5; //numero
+
+//var - let - const
+
+var primeraVariable =5
+let terceraVariable =15
+{
+var segundaVariable =10
+let cuartaVariable =20
+}
+
+
+//let {variable que cambia} vs const {variable que va a estar fija }
+
+let contador1 = 0
+let contador2 = 1
+let contador3 = 2
+const resultado=contador2+contador3*4
+//console.log (contador);
+
+const string1= 'hola'
+const string2= 'como estas'
+const string3= '?'
+
+console.log(string1+ ''+string2+string3)
+
+const nombreUsuario = prompt( 'ingrese su nombre')
+alert ("bienvenido" + nombreUsuario)
+console.log (nombreUsuario)
+
+
+//prompt() el usuario me pasa info a mi codigo
+//alert("bienvenido")darle info al usuario 
